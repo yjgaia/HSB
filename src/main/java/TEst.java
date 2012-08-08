9c2d775ba26a33a5c5af6b1d3e1e297b8b7a8786
Hanul <mr@hanul.co>
2012-08-09 08:43:49 +0900

    Redis 접속

---
+++ src/main/java/TEst.java
@@ -0,0 +1,13 @@
+import redis.clients.jedis.Jedis;
+
+
+public class TEst {
+	
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("hanul.co");
+		jedis.set("foo", "bar");
+		String value = jedis.get("foo");
+		System.out.println(value);
+	}
+
+}
