cdd5022a74d775acf48d43f67c680a47ae56aa57
oksktank <oksktank@gmail.com>
2012-08-21 17:11:57 +0900

    Follower Entity 객체 생성

---
+++ src/main/java/kr/swmaestro/hsb/model/Follower.java
@@ -0,0 +1,31 @@
+package kr.swmaestro.hsb.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+/**
+ * @author 이한솔  
+ */
+@RooJavaBean
+@RooToString
+@RooEntity
+public class Follower extends SecureKeyModel{
+
+
+	@NotNull
+	private Long userId;
+	
+	@NotNull
+	private Long followerId;
+	
+	@Column(nullable = false)
+	private Date followDate;
+
+}
